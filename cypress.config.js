9c0564c10fc4afbd53491143fb97f60ffda13a97
Tyler Biethman <tbiethman@gmail.com>
2022-06-23 17:28:05 -0500

    Revert "Adding config showing problem"
    This reverts commit 81eb19ff20878e5a94bdfe058a20e0c6c0ffc494.

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   e2e: {
-    supportFile: 'cypress/support/e2e (1).js',
-    // this also fails, due to some logic we have to work around windows paths.
-    // we think the escape characters are path separators and replace them.
-    // supportFile: 'cypress/support/e2e \\(1\\).js',
     setupNodeEvents(on, config) {},
   },
 }

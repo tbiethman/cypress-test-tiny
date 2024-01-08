64fe0e5244528bd69c08d9a4f6aec92ccd9ec2a3
Tyler Biethman <tbiethman@gmail.com>
2024-01-08 09:16:00 -0600

    Updates for flake status demo

--- cypress/e2e/baseline.cy.js
+++ cypress/e2e/baseline.cy.js
@@ -4,25 +4,25 @@ describe('baseline functionality', () => {
     expect(true).to.eq(true)
   })
 
-  it('has a failing test', () => {
-    cy.wait(500)
-    expect(true).to.eq(false)
-  })
+  // it('has a failing test', () => {
+  //   cy.wait(500)
+  //   expect(true).to.eq(false)
+  // })
 
-  // TODO: skipping for a reason
-  it.skip('has a skipped test', () => {
-    cy.wait(500)
-    expect(true).to.eq(true)
-  })
+  // // TODO: skipping for a reason
+  // it.skip('has a skipped test', () => {
+  //   cy.wait(500)
+  //   expect(true).to.eq(true)
+  // })
 
-  it('has a flaky test', { retries: 3 }, () => {
-    const attempt = cy.state('runnable')._currentRetry
+  // it('has a flaky test', { retries: 3 }, () => {
+  //   const attempt = cy.state('runnable')._currentRetry
 
-    expect(attempt).to.eq(3)
-  })
+  //   expect(attempt).to.eq(3)
+  // })
 
-  it('has another passing test', () => {
-    cy.wait(500)
-    expect(true).to.eq(true)
-  })
+  // it('has another passing test', () => {
+  //   cy.wait(500)
+  //   expect(true).to.eq(true)
+  // })
 })

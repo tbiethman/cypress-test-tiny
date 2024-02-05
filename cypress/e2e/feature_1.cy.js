c246ee6e297c28876b0a9ff2a84b7908e192259d
Tyler Biethman <tbiethman@gmail.com>
2024-02-05 14:41:51 -0600

    Update feature_1.cy.js

--- cypress/e2e/feature_1.cy.js
+++ cypress/e2e/feature_1.cy.js
@@ -3,39 +3,4 @@ describe('new feature functionality', () => {
     cy.wait(500)
     expect(true).to.eq(true)
   })
-
-  it('has a failing test', () => {
-    cy.wait(500)
-    expect(true).to.eq(false)
-  })
-
-  // TODO: skipping for a reason
-  it.skip('has a skipped test', () => {
-    cy.wait(500)
-    expect(true).to.eq(true)
-  })
-
-  it('has a flaky test', { retries: 3 }, () => {
-    const attempt = cy.state('runnable')._currentRetry
-
-    expect(attempt).to.eq(3)
-  })
-
-  it('has another passing test', () => {
-    cy.wait(500)
-    expect(true).to.eq(true)
-  })
-
-  it('has another passing test', () => {
-    cy.wait(500)
-    expect(true).to.eq(true)
-  })
-
-  it('tests milestone 2 change 1', () => {
-    expect(true).to.eq(false)
-  })
-
-  it('tests milestone 2 change 2', () => {
-    expect(true).to.eq(false)
-  })
 })
